eb45f340cc465f67c36911a3e17bd1e1c1f42bd3
Marcus Cruz <marcus.cruz85@gmail.com>
2024-03-02 21:20:30 -0700

    log env

--- infra/database.js
+++ infra/database.js
@@ -2,6 +2,7 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   console.log("> [database] queryObject", queryObject);
+  console.log("> [database] DATABASE_URL", process.env.DATABASE_URL);
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
   });
