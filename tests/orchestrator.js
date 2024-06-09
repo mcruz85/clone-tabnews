08234e916998c69ccc41902960464c9af4409430
Marcus Cruz <marcus.cruz@paygo-consult.com.br>
2024-06-08 18:28:56 -0600

    makes `npm test` more robust with `async-retry` and `orchestrator.js`

---
+++ tests/orchestrator.js
@@ -0,0 +1,27 @@
+import retry from "async-retry";
+
+async function waitForAppToStart() {
+    await waitForWebServer();
+
+    async function waitForWebServer() {
+        return retry(fetchStatusPage, {
+            retries: 100,
+            minTimeout: 2000,
+            factor: 1,
+            onRetry: (error, attempt) => console.log(`Attempt ${attempt} failed: ${error.message}`),
+        })
+
+        async function fetchStatusPage() {
+            const response = await fetch("http://localhost:3020/api/v1/status");
+            if (response.status !== 200) {
+                throw new Error("App is not ready yet");
+            }
+        }
+
+    }
+}
+
+
+export default {
+    waitForAppToStart,
+}
