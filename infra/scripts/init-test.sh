a5e2d281fce4e3c8d4e73b890c7f28be2beb7960
Marcus Cruz <marcus.cruz@paygo-consult.com.br>
2024-07-06 11:39:47 -0600

    removes sleep

--- infra/scripts/init-test.sh
+++ infra/scripts/init-test.sh
@@ -9,6 +9,6 @@ function cleanup {
 
 trap cleanup EXIT INT   
 
-npm run services:up && npm run wait-for-postgres && concurrently -n next,jest --hide next -k --success command-jest "sleep 5;next dev --port 3020" "jest --runInBand --verbose" 
+npm run services:up && npm run wait-for-postgres && concurrently -n next,jest --hide next -k --success command-jest "next dev --port 3020" "jest --runInBand --verbose" 
 
 
