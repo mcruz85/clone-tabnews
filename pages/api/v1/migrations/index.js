7a86d0516f01f80943efa07479ace5ccec350f44
Marcus Cruz <marcus.cruz@paygo-consult.com.br>
2025-06-13 19:39:37 -0300

    refactor: make /api/v1/migrations use migrator  model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,46 +1,18 @@
-import migrationRunner from "node-pg-migrate";
 import { createRouter } from "next-connect";
-import { resolve } from "node:path";
-import database from "infra/database";
 import controller from "infra/shared/controller";
+import migrator from "models/migratror";
 
 const router = createRouter();
-const DRY_RUN = true;
-const EXECUTE_MIGRATION = false;
 
-const defaultMigrationOptions = {
-  direction: process.env.MIGRATION_DIRECTION || "up",
-  migrationsTable: process.env.MIGRATIONS_TABLE || "pgmigrations",
-  dir: resolve(process.env.MIGRATIONS_DIR || "infra/migrations"),
-};
-
-async function runMigrations(dryRun) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dbClient,
-      dryRun,
-    });
-    return migratedMigrations;
-  } finally {
-    if (dbClient) await dbClient.end();
-  }
+async function getHandler(req, res) {
+  const listPendingMigrations = await migrator.listPendingMigrations();
+  return res.status(200).json(listPendingMigrations);
 }
 
-async function migrationHandler(req, res, dryRun) {
-  const migratedMigrations = await runMigrations(dryRun);
-  const status = migratedMigrations.length === 0 ? 200 : dryRun ? 200 : 201;
+async function postHandler(req, res) {
+  const migratedMigrations = await migrator.runPendingMigrations();
+  const status = migratedMigrations.length === 0 ? 200 : 201;
   res.status(status).json(migratedMigrations);
-}
-
-function getHandler(req, res) {
-  return migrationHandler(req, res, DRY_RUN);
-}
-
-function postHandler(req, res) {
-  return migrationHandler(req, res, EXECUTE_MIGRATION);
 }
 
 router.get(getHandler);
