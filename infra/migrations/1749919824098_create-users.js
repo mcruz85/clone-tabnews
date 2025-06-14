1ea8ae856763f32047f296660c04a1cb1a4e85a2
Marcus Cruz <marcus.cruz@paygo-consult.com.br>
2025-06-14 16:56:45 -0300

    feat: add migration to create  `users` table

---
+++ infra/migrations/1749919824098_create-users.js
@@ -0,0 +1,58 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+    },
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+    },
+    password: {
+      type: "varchar(60)",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+  });
+
+  pgm.createIndex("users", "LOWER(username)", {
+    name: "idx_users_username_lower",
+    unique: true,
+  });
+
+  pgm.createIndex("users", "LOWER(email)", {
+    name: "idx_users_email_lower",
+    unique: true,
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = false;
