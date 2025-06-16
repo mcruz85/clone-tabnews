fd55bc1986a43c4afa7abbf47699cc441fddf211
Marcus Cruz <marcus.cruz@paygo-consult.com.br>
2025-06-16 17:34:19 -0300

    fix: dir configuration of migrations

--- models/migratror.js
+++ models/migratror.js
@@ -6,7 +6,7 @@ import { ServiceError } from "infra/errors";
 const defaultMigrationOptions = {
   direction: process.env.MIGRATION_DIRECTION || "up",
   migrationsTable: process.env.MIGRATIONS_TABLE || "pgmigrations",
-  dir: resolve(process.env.MIGRATIONS_DIR || "infra/migrations"),
+  dir: resolve("infra", "migrations"),
   log: () => {},
 };
 
