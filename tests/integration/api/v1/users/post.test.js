2e79edb7ca37dd069bd1cc9722445652719669e3
Marcus Cruz <marcus.cruz@paygo-consult.com.br>
2025-06-18 19:15:10 -0300

    test: use shared `createUser ` helper and improve user creation tests

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -2,21 +2,13 @@ import orchestrator from "tests/orchestrator.js";
 import { version as uuidVersion } from "uuid";
 import database from "infra/database";
 import password from "models/password";
+import { createUser } from "tests/helpers/user.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAppToStart();
   await orchestrator.cleanDatabase();
   await orchestrator.runPendingMigrations();
 });
-
-async function createUser({ username, email, password }) {
-  const response = await fetch("http://localhost:3020/api/v1/users", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username, email, password }),
-  });
-  return response;
-}
 
 describe("POST /api/v1/users", () => {
   describe("Anonymous user", () => {
@@ -147,85 +139,6 @@ describe("POST /api/v1/users", () => {
           expect(responseBody).toEqual(expectedError);
         },
       );
-    });
-  });
-});
-
-describe("POST /api/v1/users/[username]", () => {
-  describe("Anonymous user", () => {
-    test("With exact case match", async () => {
-      const response = await createUser({
-        username: "MesmoCase",
-        email: "mesmo.case@mail.com",
-        password: "P@ssw0rd",
-      });
-      expect(response.status).toBe(201);
-
-      const response2 = await fetch(
-        "http://localhost:3020/api/v1/users/MesmoCase",
-      );
-
-      expect(response2.status).toBe(200);
-
-      const response2Body = await response2.json();
-
-      expect(response2Body).toEqual({
-        id: response2Body.id,
-        username: "MesmoCase",
-        email: "mesmo.case@mail.com",
-        created_at: response2Body.created_at,
-        updated_at: response2Body.updated_at,
-      });
-
-      expect(uuidVersion(response2Body.id)).toBe(4);
-      expect(Date.parse(response2Body.created_at)).not.toBeNaN();
-      expect(Date.parse(response2Body.updated_at)).not.toBeNaN();
-    });
-
-    test("With case missmatch", async () => {
-      const response = await createUser({
-        username: "CaseInsensitive",
-        email: "case.insensitive@mail.com",
-        password: "P@ssw0rd",
-      });
-      expect(response.status).toBe(201);
-
-      const response2 = await fetch(
-        "http://localhost:3020/api/v1/users/caseinsensitive",
-      );
-
-      expect(response2.status).toBe(200);
-
-      const response2Body = await response2.json();
-
-      expect(response2Body).toEqual({
-        id: response2Body.id,
-        username: "CaseInsensitive",
-        email: "case.insensitive@mail.com",
-        created_at: response2Body.created_at,
-        updated_at: response2Body.updated_at,
-      });
-
-      expect(uuidVersion(response2Body.id)).toBe(4);
-      expect(Date.parse(response2Body.created_at)).not.toBeNaN();
-      expect(Date.parse(response2Body.updated_at)).not.toBeNaN();
-    });
-
-    test("Non-existing username", async () => {
-      const response = await fetch(
-        "http://localhost:3020/api/v1/users/unknownuser",
-      );
-
-      expect(response.status).toBe(404);
-
-      const responseBody = await response.json();
-
-      expect(responseBody).toEqual({
-        name: "NotFoundError",
-        message: "User not found",
-        action: "Please check the username and try again",
-        status_code: 404,
-      });
     });
   });
 });
